875e2def68f30a958b962cc785b46283c92312e3
wenblack <wenblack012@gmail.com>
2022-10-25 17:37:22 -0300

    Add icons for Tab navigation bar

--- Routes/index.tsx
+++ Routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { Home } from '../screens/Home';
 import { Ranking } from '../screens/Ranking';
@@ -10,8 +11,27 @@ const Tab = createBottomTabNavigator();
 
 export function Routes() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown:false}}>
+    <NavigationContainer >
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name='radio-button-off' size={36} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Ranking" component={Ranking} />
         <Tab.Screen name="WIki" component={WIki} />
